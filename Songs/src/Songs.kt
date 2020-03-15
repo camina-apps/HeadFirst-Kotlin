3e900df8c9a05e167f9ebf37de2086edc57b59ea
Carmen Probst <camina.apps@gmail.com>
2020-03-15 19:48:24 +0100

    first few chapters

---
+++ Songs/src/Songs.kt
@@ -0,0 +1,19 @@
+class Song(val title: String, val artist: String) {
+    fun play() {
+        println("Playing the song $title by $artist")
+    }
+
+    fun stop() {
+        println("Stopped playing $title")
+    }
+}
+
+
+fun main() {
+    val songOne = Song("The Mesoptamians", "They Might Be Giants")
+    val songTwo = Song("Going Underground", "The Jam")
+    val songThree = Song("Make Me Smile", "Steve Harey")
+    songTwo.play()
+    songTwo.stop()
+    songThree.play()
+}
