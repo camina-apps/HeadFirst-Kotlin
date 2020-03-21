3f9c66f520b51865baa8986d4e6ccf488d55a777
Carmen Probst <camina.apps@gmail.com>
2020-03-21 19:49:52 +0100

    abstract classes

--- Animals/src/Animal.kt
+++ Animals/src/Animal.kt
@@ -1,19 +1,15 @@
 
-open class Animal {
-    open val image = ""
-    open val food = ""
-    open val habitat = ""
+abstract class Animal : Roamable {
+    abstract val image: String
+    abstract val food: String
+    abstract val habitat: String
     open var hunger = 10
 
-    open fun makeNoise() {
-        println("The Animal is making a noise")
-    }
+    abstract fun makeNoise()
 
-    open fun eat() {
-        println("The Animal is eating")
-    }
+    abstract fun eat()
 
-    open fun roam() {
+    override fun roam() {
         println("The Animal is roaming")
     }
 
--- Animals/src/Canine.kt
+++ Animals/src/Canine.kt
@@ -1,5 +1,5 @@
 
-open class Canine : Animal() {
+abstract class Canine : Animal() {
     override fun roam() {
         println("The Canine is roaming")
     }
